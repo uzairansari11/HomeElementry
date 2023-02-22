1d4cf0e351eae867828ae5e6c26546c3f49eef8a
singh2198 <8755akash@gmail.com>
2023-02-22 23:37:32 +0530

    day_2@akash

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import {Heading } from "@chakra-ui/react"
 import './App.css';
+import Productpages from "./Component/Productpage"
+
 
 function App() {
   return (
     <div className="App">
      <Heading>Hello Rct-201   Project</Heading>
+     <Productpages/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { Provider } from 'react-redux';
+import { store } from './Redux/Store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<ChakraProvider>
+    
+    <ChakraProvider>
+    <Provider store={store}>
     <App />
-    </ChakraProvider>
+    </Provider>
+</ChakraProvider>
  
 );
 
