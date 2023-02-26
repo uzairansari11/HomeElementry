48f4cbc74221e7847d02266caa79f102f942c58e
Uzair Ansari <uzairans532@gmail.com>
2023-02-26 13:34:29 +0530

    updated cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react';
+import { Grid, Text } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CartCard } from '../Components/CartCard';
@@ -24,12 +24,22 @@ console.log(allCartData)
     <div>
   
       
+<Grid
+  justifyContent={"space-around"}
+      templateColumns={{ base: `repeat(1, 1fr)`,sm: `repeat(2,1fr)`,md: `repeat(2,1fr)`}}
+      gap={2} >
 
       {allCartData.length?
 
-      allCartData.map((cartItem)=><CartCard   key={cartItem.id}  {...cartItem} />):
+      allCartData.map((cartItem)=>
+      
+      
+      <CartCard   key={cartItem.id}  {...cartItem} />):
       
        <Text as="b" fontSize={40} color="red">No Items in Cart</Text>}
+
+</Grid>
+       
     </div>
   );
 }
